35ae56dba5c3303e4f0d9b0e3cbcd7bbe47e855b
Ole Hansen <ole.hansen@fortis-it.de>
2019-06-04 14:01:26 +0200

    Add init configs script

---
+++ .config/dots/init_configs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo '#!/bin/bash' > ~/.config/wallpaper/set
+echo 'swaymsg output "*" background ~/.config/wallpaper/background.png fill' >> ~/.config/wallpaper/set
+chmod +x ~/.config/wallpaper/set
+cp ~/.config/dots/proxy.sample ~/.config/dots/proxy
+touch ~/.config/dots/gitconfig.local
+touch ~/.config/dots/zshrc.local
