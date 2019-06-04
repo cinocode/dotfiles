855b22fa4252a02339d0bfab3b85c8ea70fbb5ba
Ole Hansen <ole.hansen@fortis-it.de>
2019-06-04 09:16:14 +0200

    Switch dia script to symbolic link

--- .bin/dia.py
+++ .bin/dia.py
@@ -6,4 +6,4 @@ for x in range(int(sys.argv[2])):
       for file in files:
           if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png") or file.endswith(".bmp") or file.endswith(".gif"):
               extension = os.path.splitext(file)[-1]
-              os.system("ln '" + os.path.join(root, file) + "' '/home/ole/dia/" + str(random.randint(0, sys.maxsize)) + extension + "'")
+              os.system("ln -s '" + os.path.join(root, file) + "' '/home/ole/dia/" + str(random.randint(0, sys.maxsize)) + extension + "'")
