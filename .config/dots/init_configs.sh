c7d499f9f719f379903e3e7a8cc11905f30c9289
Ole Hansen <ole.hansen@fortis-it.de>
2019-06-06 14:55:16 +0200

    Move workspace bindings to config local

--- .config/dots/init_configs.sh
+++ .config/dots/init_configs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sh ~/.config/sway/setup
 echo '#!/bin/bash' > ~/.config/wallpaper/set
 echo 'swaymsg output "*" background ~/.config/wallpaper/background.png fill' >> ~/.config/wallpaper/set
 chmod +x ~/.config/wallpaper/set
