b739082b1fdba10acc478914374eb9ccb05e83f3
Ole Hansen <ole.hansen@fortis-it.de>
2019-06-07 18:47:07 +0200

    Simplify locking script

--- .bin/lock.sh
+++ .bin/lock.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-swaylock-blur -- -K -f --font Inconsolata --ring-color 11111111 --ring-clear-color 11111111 --key-hl-color 11111199 --inside-color 99999900
+swaylock-blur -- -f -K -u --font Inconsolata
