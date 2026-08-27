fd9916443ce08966f3d8a3f519dde1d27795dea3
Ole Hansen <ole.hansen@fortis-it.de>
2026-08-27 13:29:11 +0200

    chore: preview m3u files as text in ranger

--- .config/ranger/scope.sh
+++ .config/ranger/scope.sh
@@ -62,6 +62,8 @@ handle_extension() {
             atool --list -- "${FILE_PATH}" && exit 5
             bsdtar --list --file "${FILE_PATH}" && exit 5
             exit 1;;
+        m3u)
+          exit 2;;
         rar)
             ## Avoid password prompt by providing empty password
             unrar lt -p- -- "${FILE_PATH}" && exit 5
